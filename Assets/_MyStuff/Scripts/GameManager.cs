f04b8ae057004e3c8e07de1e1e4eacdf10472f95
Claus Bjerre Krogh <claus@aneks.dk>
2024-09-01 14:28:09 +0200

    Unity project added to repo

---
+++ Assets/_MyStuff/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public TextMeshProUGUI pickupText;
+    string scoreText = "Pickup count";
+    int pickupCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickupText.text = scoreText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPickedUp()
+    {
+        pickupCount += 1;
+
+        pickupText.text = scoreText + " " + pickupCount.ToString();
+    }
+}
---
+++ Assets/_MyStuff/Scripts/PlayerControl.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 5;
+    public float jumpForce = 5;
+
+    float zMovement;
+    float xMovement;
+    Vector3 movement;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zMovement = Input.GetAxis("Vertical");
+        xMovement = Input.GetAxis("Horizontal");
+        movement = new Vector3(xMovement, 0, zMovement);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(movement * speed);
+    }
+}
